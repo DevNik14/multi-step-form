4515f4708f38801b1ba9a32015963c16a5ea7e11
DevNik14 <nykyurchiyski@gmail.com>
2024-12-07 22:13:06 +0200

    style: style the "Personal Info" step

--- src/components/FormContainer/FormContainer.tsx
+++ src/components/FormContainer/FormContainer.tsx
@@ -6,22 +6,22 @@ export default function FormContainer() {
     <>
       <section className={`${styles.formContainer}`}>
         <FormNavigation />
-        {/* <div className="formContent">
-          <header>
+        <div className={styles.formContent}>
+          <header className={styles.formHeader}>
             <h1>Personal Info</h1>
             <p>Please provide your name, email address, and phone number</p>
           </header>
           <form>
             <div>
-              <div>
+              <div className={styles.inputHolder}>
                 <label htmlFor="name">Name</label>
                 <input type="text" id="name" name="name" />
               </div>
-              <div>
+              <div className={styles.inputHolder}>
                 <label htmlFor="email">Email Address</label>
                 <input type="text" id="email" name="email" />
               </div>
-              <div>
+              <div className={styles.inputHolder}>
                 <label htmlFor="phone">Phone Number</label>
                 <input
                   type="text"
@@ -30,10 +30,10 @@ export default function FormContainer() {
                   placeholder="e.g. + 1 234 567 890"
                 />
               </div>
-              <button>Next Step</button>
+              <button className={styles.nextBtn}>Next Step</button>
             </div>
           </form>
-        </div> */}
+        </div>
       </section>
     </>
   );
