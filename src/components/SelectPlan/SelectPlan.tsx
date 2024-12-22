abb44b69597e34b3bae7ad4c4eba0d64c487e5c6
Nikolay Kyurchiyski <nykyurchiyski@gmail.com>
2024-12-22 20:08:20 +0200

    24 style select plan page (#27)
    style: create select plan layout

--- src/components/SelectPlan/SelectPlan.tsx
+++ src/components/SelectPlan/SelectPlan.tsx
@@ -8,7 +8,45 @@ export default function SelectPlan() {
         <h1>Select Your Plan</h1>
         <p>You have the option for monthly of yearly billing.</p>
       </FormHeader>
-      <div></div>
+      <div className={styles.priceInfo}>
+        <div className="planItem">
+          <div className="planIcon">
+            <img src="images/icon-arcade.svg" alt="arcade" />
+          </div>
+          <div className="planPrice">
+            <p>Arcade</p>
+            <p>$9/mo</p>
+          </div>
+          div
+        </div>
+        <div className="planItem">
+          <div className="planIcon">
+            <img src="images/icon-advanced.svg" alt="advanced" />
+          </div>
+          <div className="planPrice">
+            <p>Advanced</p>
+            <p>$12/mo</p>
+          </div>
+          div
+        </div>
+        <div className="planItem">
+          <div className="planIcon">
+            <img src="images/icon-pro.svg" alt="pro" />
+          </div>
+          <div className="planPrice">
+            <p>Pro</p>
+            <p>$15/mo</p>
+          </div>
+        </div>
+      </div>
+      <div className="subscriptionType">
+        <div className="monthly">Monthly</div>
+        <label className="switch">
+          <input type="checkbox" />
+          <span className="slider round"></span>
+        </label>
+        <div className="yearly">Yearly</div>
+      </div>
     </>
   );
 }
