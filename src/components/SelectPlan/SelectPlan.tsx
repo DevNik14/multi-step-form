b68c4ccf3efa963b2f9d1f9079a765f65a17e1f1
DevNik14 <nykyurchiyski@gmail.com>
2024-12-23 21:45:43 +0200

    style: sthe desktop view

--- src/components/SelectPlan/SelectPlan.tsx
+++ src/components/SelectPlan/SelectPlan.tsx
@@ -9,43 +9,41 @@ export default function SelectPlan() {
         <p>You have the option for monthly of yearly billing.</p>
       </FormHeader>
       <div className={styles.priceInfo}>
-        <div className="planItem">
-          <div className="planIcon">
+        <div className={`${styles.planItem} ${styles.selected}`}>
+          <div className={styles.planIcon}>
             <img src="images/icon-arcade.svg" alt="arcade" />
           </div>
-          <div className="planPrice">
-            <p>Arcade</p>
+          <div className={styles.planPrice}>
+            <p className={styles.planTitle}>Arcade</p>
             <p>$9/mo</p>
           </div>
-          div
         </div>
-        <div className="planItem">
-          <div className="planIcon">
+        <div className={styles.planItem}>
+          <div className={styles.planIcon}>
             <img src="images/icon-advanced.svg" alt="advanced" />
           </div>
-          <div className="planPrice">
-            <p>Advanced</p>
+          <div className={styles.planPrice}>
+            <p className={styles.planTitle}>Advanced</p>
             <p>$12/mo</p>
           </div>
-          div
         </div>
-        <div className="planItem">
-          <div className="planIcon">
+        <div className={styles.planItem}>
+          <div className={styles.planIcon}>
             <img src="images/icon-pro.svg" alt="pro" />
           </div>
-          <div className="planPrice">
-            <p>Pro</p>
+          <div className={styles.planPrice}>
+            <p className={styles.planTitle}>Pro</p>
             <p>$15/mo</p>
           </div>
         </div>
       </div>
-      <div className="subscriptionType">
-        <div className="monthly">Monthly</div>
-        <label className="switch">
+      <div className={styles.subscriptionType}>
+        <div className={styles.monthly}>Monthly</div>
+        <label className={styles.switch}>
           <input type="checkbox" />
-          <span className="slider round"></span>
+          <span className={`${styles.slider} ${styles.round}`}></span>
         </label>
-        <div className="yearly">Yearly</div>
+        <div className={styles.yearly}>Yearly</div>
       </div>
     </>
   );
