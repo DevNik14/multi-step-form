2fe22bbea9675154e9eb61f1091c06e4871ffde7
DevNik14 <nykyurchiyski@gmail.com>
2024-12-25 21:53:30 +0200

    feat: add bo back/next functionality

--- src/components/FormButtons/NextButton.tsx
+++ src/components/FormButtons/NextButton.tsx
@@ -1,5 +1,22 @@
+import { Link } from "react-router";
+
+import { navItems } from "../../navData";
+
 import styles from "./FormButtons.module.scss";
 
 export default function NextButton() {
-  return <button className={styles.nextBtn}>Next Step</button>;
+  const currentPagePath = window.location.pathname.split("/")[1];
+  const currentPagePathIndex = navItems.findIndex(
+    (navItem) => navItem.path === currentPagePath
+  );
+  const nextPath = navItems[currentPagePathIndex + 1]?.path;
+  const displayNextStepLinkOrConfirmButton = nextPath ? (
+    <Link to={nextPath} className={styles.nextBtn}>
+      Next Step
+    </Link>
+  ) : (
+    <button className={styles.nextBtn}>Confirm</button>
+  );
+
+  return <>{displayNextStepLinkOrConfirmButton}</>;
 }
--- src/components/SelectPlan/SelectPlan.tsx
+++ src/components/SelectPlan/SelectPlan.tsx
@@ -16,7 +16,7 @@ export default function SelectPlan() {
         }
       };
     }
-  }, []);
+  }, [window.innerWidth]);
 
   return (
     <>
