83e37e307fce04020d5e046b41e8b44e56c26dfb
Nikolay Kyurchiyski <nykyurchiyski@gmail.com>
2024-12-30 15:42:06 +0200

    36 implement form state management (#42)
    feat: make the Summary page dynamic

--- src/components/Summary/Summary.tsx
+++ src/components/Summary/Summary.tsx
@@ -2,13 +2,25 @@ import FormHeader from "../FormHeader/FormHeader";
 import { useMultiStepForm } from "../../contexts/MultiStepFormContext";
 import { addons } from "../../data";
 import styles from "./Summary.module.scss";
+import { useEffect, useState } from "react";
+import { Link } from "react-router";
 
 export default function Summary() {
+  const [total, setTotal] = useState(0);
   const multiStepForm = useMultiStepForm();
   const period = multiStepForm?.formValues.period;
   const planName = multiStepForm?.formValues.planName;
   const formattedPeriod =
     multiStepForm?.formValues.period === "monthly" ? "mo" : "yr";
+
+  const planNameAsKey = (
+    key: string | undefined
+  ): "arcade" | "advanced" | "pro" => {
+    if (key === "advanced" || key === "pro") {
+      return key;
+    }
+    return "arcade";
+  };
 
   const periodAsKey = (key: string | undefined): "monthly" | "yearly" => {
     if (key === "yearly") {
@@ -17,11 +29,30 @@ export default function Summary() {
     return "monthly";
   };
 
+  useEffect(() => {
+    const servicesPrices =
+      getSelectedServices().length > 0
+        ? getSelectedServices().reduce((totalPrice, servicePrice) => {
+            return totalPrice + Number(servicePrice[1]);
+          }, 0)
+        : 0;
+    const planPrice =
+      multiStepForm?.formValues.planType[planNameAsKey(planName)];
+    if (planPrice) {
+      setTotal(planPrice + servicesPrices);
+    } else {
+      setTotal(9 + servicesPrices);
+    }
+  }, []);
+
   const getSelectedServices = () => {
-    return Object.entries(multiStepForm?.formValues)
+    return Object.entries(multiStepForm?.formValues || {})
       .slice(-3)
       .filter((item) => item[1] !== false)
-      .map((item) => [item[0], addons[item[0]][period]]);
+      .map((item) => [
+        item[0],
+        addons[item[0] as keyof typeof addons][periodAsKey(period)],
+      ]);
   };
 
   const displaySelectedServices = () => {
@@ -50,10 +81,12 @@ export default function Summary() {
               {multiStepForm?.formValues.planName} (
               {multiStepForm?.formValues.period})
             </p>
-            <p className={styles.changeSummaryPlan}>Change</p>
+            <Link to="/select-plan">
+              <p className={styles.changeSummaryPlan}>Change</p>
+            </Link>
           </div>
           <p className={styles.planSummaryTitle}>
-            $/{multiStepForm?.formValues.planType[planName]}
+            $/{multiStepForm?.formValues.planType[planNameAsKey(planName)]}
             {formattedPeriod}
           </p>
         </div>
@@ -63,8 +96,10 @@ export default function Summary() {
         </div>
       </div>
       <div className={styles.summaryTotalPriceInfo}>
-        <p className={styles.summaryTotal}>Total (per month)</p>
-        <p className={styles.summaryPrice}>+12$/mo</p>
+        <p className={styles.summaryTotal}>
+          Total (per {period?.slice().replace("ly", "")})
+        </p>
+        <p className={styles.summaryPrice}>+{total}$/mo</p>
       </div>
     </>
   );
