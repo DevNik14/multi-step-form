f113c3e9eadb9dd4820eee326a86e088174c2c03
Nikolay Kyurchiyski <nykyurchiyski@gmail.com>
2025-01-02 23:41:17 +0200

    36 implement form state management (#44)
    feat: added validation for required fields and disable "next step" if fields are not filled correctly

--- src/components/FormButtons/NextButton.tsx
+++ src/components/FormButtons/NextButton.tsx
@@ -3,20 +3,36 @@ import { Link } from "react-router";
 import { navItems } from "../../data";
 
 import styles from "./FormButtons.module.scss";
+import { useMultiStepForm } from "../../contexts/MultiStepFormContext";
 
 export default function NextButton() {
+  const multiStepForm = useMultiStepForm();
+
   const currentPagePath = window.location.pathname.split("/")[1];
   const currentPagePathIndex = navItems.findIndex(
     (navItem) => navItem.path === currentPagePath
   );
   const nextPath = navItems[currentPagePathIndex + 1]?.path;
-  const displayNextStepLinkOrConfirmButton = nextPath ? (
-    <Link to={nextPath} className={styles.nextBtn}>
-      Next Step
-    </Link>
-  ) : (
-    <button className={styles.nextBtn}>Confirm</button>
-  );
+  const displayButton = () => {
+    if (currentPagePath === "personal-info") {
+      return (
+        <button
+          className={styles.nextBtn}
+          onClick={multiStepForm?.checkInputValueHandler}
+        >
+          Next Step
+        </button>
+      );
+    } else if (nextPath) {
+      return (
+        <Link to={nextPath} className={styles.nextBtn}>
+          Next Step
+        </Link>
+      );
+    } else if (!nextPath) {
+      return <button className={styles.nextBtn}>Confirm</button>;
+    }
+  };
 
-  return <>{displayNextStepLinkOrConfirmButton}</>;
+  return <>{displayButton()}</>;
 }
