3d78b9a3339514e1a1b4714248af3f58bd7c4b9a
DevNik14 <nykyurchiyski@gmail.com>
2025-01-04 21:05:38 +0200

    feat: add route guard to validate personal info before proceeding

--- src/Layout/Layout.tsx
+++ src/Layout/Layout.tsx
@@ -1,10 +1,26 @@
+import { useEffect } from "react";
+
+import { useNavigate } from "react-router";
+
+import { useMultiStepForm } from "../contexts/MultiStepFormContext";
 import FormContainer from "../components/FormContainer/FormContainer";
 import MobileButtons from "../components/FormButtons/MobileButtons";
-import { useMultiStepForm } from "../contexts/MultiStepFormContext";
 import MobileNavigation from "../components/MobileNavigation/MobileNavigation";
 
 export default function Layout() {
   const multiStepForm = useMultiStepForm();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const currentPagePath = window.location.pathname.split("/")[1];
+    if (
+      currentPagePath !== "personal-info" &&
+      multiStepForm?.formValues.personalInfoError
+    ) {
+      navigate("/personal-info");
+    }
+  }, [window.location]);
+
   return (
     <>
       <MobileNavigation />
